7ee18b73f0d0de170113c5ba34aa4ec636ebf889
xr <xr@wolvestribe.com>
2018-05-07 10:09:11 +0800

    modify some details

--- pages/choseaddress/choseaddress.js
+++ pages/choseaddress/choseaddress.js
@@ -105,5 +105,11 @@ Page({
 
   radioChange: function (e) {
     console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+
+  gotourl: function(){
+    wx.redirectTo({
+      url: '/pages/addaddress/addaddress'
+    })
   }
 })
